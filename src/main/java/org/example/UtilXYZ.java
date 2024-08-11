1dd71012c822ae9b2dd324eef0d7b6284f342997
nirmith.shetty <nirmith.shetty@phonepe.com>
2024-08-12 00:28:53 +0530

    addressed the review comment

--- src/main/java/org/example/UtilXYZ.java
+++ src/main/java/org/example/UtilXYZ.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class UtilXYZ {
     public static void main(String[] args) {
-        System.out.println("XYX utility");
+        System.out.println("XYX statement changed");
     }
 }
